6d0143ba56065ef8273c9b442457c19025725287
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-26 14:10:14 +0300

    add header components

---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+  return (
+    <div>
+        <div className="bg-local" >
+<img 
+className="w-screen"
+src="./src/assets/Screen Shot 2023-11-26 at 10.49.03 AM_cleanup.png"/>
+<h2>FreshGoods</h2>
+<h1>Concisely describe your product or service</h1>
+<p>no need to get clever.Tall peop;e exaclty what you'r offering,then use this space to communicate your key value propostion</p>
+   <button  className="bg-[#fcb305] btn ">ORDER NOW</button>
+    </div>
+    </div>
+
+  )
+}
+
+export default Header
