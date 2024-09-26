08c4cad6ca8661a6adf86534afe53ca077a4a4e2
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-26 14:23:19 +0300

    add component benefitet

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
+import Benefits from './components/Benefits'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     <Header/>
+      <Header />
+      <Benefits />
     </>
   )
 }
---
+++ my-project/src/components/Benefits.jsx
@@ -0,0 +1,57 @@
+
+function Benefits() {
+  return (
+    <div className="text-center">
+        <h1 className="font-bold text-lg">Here are some of the benefits of your offer</h1>
+        <p>Explain What makes your or service great.Tlk about featuers in a way that highlights the real value people get out of them</p>
+        <div className=" flex justify-center justify-around">
+        <div className="card shadow-md shadow-black ">
+            <div className="card-body">
+            <img
+            className="img-top" 
+            src="./src/assets/Untitled(3).png" 
+
+            />
+            <h4 className="card-title">Benefit 1</h4>
+            <p className="card-text"> for example returen</p>
+            
+</div>
+
+
+        </div>
+        <div className="card shadow-md shadow-black">
+            <div className="card-body">
+            <img
+            className="img-top" 
+            src="./src/assets/Untitled(1).png" 
+
+            />
+            <h4 className="card-title">Benefit 2</h4>
+            <p className="card-text"> for example returen</p>
+            
+</div>
+
+
+        </div>
+        <div className="card shadow-md shadow-black">
+            <div className="card-body">
+            <img
+            className="img-top" 
+            src="./src/assets/Untitled(2).png" 
+
+            />
+            <h4 className="card-title">Benefit 3</h4>
+            <p className="card-text"> for example returen</p>
+            
+</div>
+
+
+        </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Benefits
