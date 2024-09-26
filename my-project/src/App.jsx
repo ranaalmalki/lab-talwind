a477568151b89d98bbebb3ac2ef653317e110951
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-26 14:54:55 +0300

    add section inforamtion

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Benefits from './components/Benefits'
 import CusomerSay from './components/CusomerSay'
 import Featuer from './components/Featuer'
+import Inforamtion from './components/Inforamtion'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -13,6 +14,8 @@ function App() {
       <Benefits />
       <CusomerSay/>
       <Featuer/>
+      <Inforamtion/>
+
       <Footer/>
     </>
   )
---
+++ my-project/src/components/Inforamtion.jsx
@@ -0,0 +1,21 @@
+
+function Inforamtion() {
+  return (
+    <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="./src/assets/Untitled(8).png"
+      alt="women" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Some more information about your business</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+  
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Inforamtion
